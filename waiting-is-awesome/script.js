f353a787cfbe0184b9f8ce93b39934329a3395b8
sankalp6115 <sankalpomar6115@gmail.com>
2025-08-09 01:34:23 +0530

    Added files for waiting-is-awesome

---
+++ waiting-is-awesome/script.js
@@ -0,0 +1,43 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    const loader = entry.target;
+
+    if (entry.isIntersecting) {
+      loader.classList.remove('paused');
+    } else {
+      loader.classList.add('paused');
+    }
+  });
+}, {
+  root: null, // viewport
+  threshold: 0.6 // adjust as needed
+});
+
+// Observe all .loader elements
+document.querySelectorAll('.loader').forEach(loader => {
+  observer.observe(loader);
+});
+
+
+//About section typewriter animation
+const text = "Free Library of Awesome Loaders!";
+const about = document.querySelector(".free-loader");
+let i=0;
+    
+    function updateText(){
+        if(i < text.length){
+            about.textContent += text.charAt(i);
+            i++
+        }
+        else{
+            clearInterval(interval);
+        }
+    }
+    const interval = setInterval(updateText, 50);
+    updateText();
+
+
+const loaders = document.querySelectorAll("section.loaders a");
+loaders.forEach(loader => {
+  loader.href = "Loaders_for_Website/";
+});
