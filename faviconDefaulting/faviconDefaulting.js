90e1a6c315c52fc86562356ad4e3987b026de1c6
sankalp6115 <sankalpomar6115@gmail.com>
2025-08-11 17:04:34 +0530

    Added daefault favicon system

--- faviconDefaulting/faviconDefaulting.js
+++
@@ -1,7 +0,0 @@
-export function setFavicon(){
-    const link = document.createElement("link");
-    link.rel = "shortcut icon";
-    link.href = "https://i.ibb.co/99Sndx5j/icon.png";
-    link.type = "image/x-icon";
-    document.head.appendChild(link);
-}
